ea52c0fa4e3423602c59cd77eb187f65061f7236
esonray <sonia.raychaudhuri@ericsson.com>
2016-02-03 11:41:01 +0530

    modifying category page

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,23 +1,27 @@
-var app = angular.module("myapp", ['ngRoute']);
-app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-	    .when('/Home', {
-	      templateUrl: 'HomePage.html',
-	      controller: 'ProductController'
-	   }).
-	   
-	   when('/Contact', {
-	      templateUrl: 'contact.html',
-	      controller: 'ContactController'
-	   }).
-	   
-	   otherwise({
-	      redirectTo: '/Home'
-	   });
-}]);
+var app = angular.module("myapp", [ 'ngRoute' ]);
+app.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/Home', {
+		templateUrl : 'HomePage.html',
+		controller : 'ProductController'
+	}).
 
-app.factory('filters', function () {
-    return { category: '' };
+	when('/Contact', {
+		templateUrl : 'contact.html',
+		controller : 'ContactController'
+	}).
+
+	when('/category', {
+		templateUrl : 'category.html',
+		controller : 'ProductController'
+	}).
+
+	otherwise({
+		redirectTo : '/Home'
+	});
+} ]);
+
+app.factory('filters', function() {
+	return {
+		category : ''
+	};
 });
-
-

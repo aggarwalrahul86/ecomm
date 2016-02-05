cdee56a27d9242ef295df325b085541c3200cf79
esonray <sonia.raychaudhuri@ericsson.com>
2016-02-05 16:17:25 +0530

    angularjs-pagination and array of json implementation

--- src/main/resources/controllers/ProductController.js
+++ src/main/resources/controllers/ProductController.js
@@ -1,6 +1,7 @@
 app.controller("ProductController", function($scope,$http,filters,productService) {
 	
-	$scope.filters = filters; 
+	$scope.filters = filters;
+	$scope.rowsPerPage = 50;
 	
 	$scope.categories = productService.getProductCategories().success(
 			function(response) {
@@ -8,7 +9,7 @@ app.controller("ProductController", function($scope,$http,filters,productService
 			});
     
     $http.get("../json/ProductConfig.json").success( function(response) {
-        $scope.products = response; 
+        $scope.products = response;
      });
     
     $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
@@ -17,7 +18,6 @@ app.controller("ProductController", function($scope,$http,filters,productService
          carousels();
          utils();
          demo();
-         console.log("visibility is"+ $('.product-slider .item').css('visibility'));
     });
  
     
--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -30,22 +30,127 @@ app.factory('filters', function() {
 	};
 });
 
-app.directive('onFinishRender', function ($timeout) {
-return {
-    restrict: 'A',
-    link: function (scope, element, attr) {
-        if (scope.$last === true) {
-            $timeout(function () {
-                scope.$emit('ngRepeatFinished');
-            });
-        }
-    }
-}
-});
-
-app.factory('filters', function() {
+app.directive('onFinishRender', function($timeout) {
 	return {
-		category : ''
-	};
+		restrict : 'A',
+		link : function(scope, element, attr) {
+			if (scope.$last === true) {
+				$timeout(function() {
+					scope.$emit('ngRepeatFinished');
+				});
+			}
+		}
+	}
 });
+/*
+ * app.directive('carouselDirective', function() { return function(scope,
+ * element, attrs) { console.log('ROW: index = ', scope.$index);
+ * scope.$watch('$last',function(v){ carou });
+ *  }; })
+ */
 
+app.filter('paginate', function(Paginator) {
+    return function(input, rowsPerPage) {
+        if (!input) {
+            return input;
+        }
+
+        if (rowsPerPage) {
+            Paginator.rowsPerPage = rowsPerPage;
+        }
+        
+        Paginator.itemCount = input.length;
+
+        return input.slice(parseInt(Paginator.page * Paginator.rowsPerPage), parseInt((Paginator.page + 1) * Paginator.rowsPerPage + 1) - 1);
+    }
+
+})
+
+.filter('forLoop', function() {
+    return function(input, start, end) {
+        input = new Array(end - start);
+        for (var i = 0; start < end; start++, i++) {
+            input[i] = start;
+        }
+
+        return input;
+    }
+})
+
+
+.service('Paginator', function ($rootScope) {
+    this.page = 0;
+    this.rowsPerPage = 50;
+    this.itemCount = 0;
+    this.limitPerPage = 5;
+
+    this.setPage = function (page) {
+        if (page > this.pageCount()) {
+            return;
+        }
+
+        this.page = page;
+    };
+
+    this.nextPage = function () {
+        if (this.isLastPage()) {
+            return;
+        }
+
+        this.page++;
+    };
+
+    this.perviousPage = function () {
+        if (this.isFirstPage()) {
+            return;
+        }
+
+        this.page--;
+    };
+
+    this.firstPage = function () {
+        this.page = 0;
+    };
+
+    this.lastPage = function () {
+        this.page = this.pageCount() - 1;
+    };
+
+    this.isFirstPage = function () {
+        return this.page == 0;
+    };
+
+    this.isLastPage = function () {
+        return this.page == this.pageCount() - 1;
+    };
+
+    this.pageCount = function () {
+        return Math.ceil(parseInt(this.itemCount) / parseInt(this.rowsPerPage));
+    };
+    
+    this.lowerLimit = function() { 
+        var pageCountLimitPerPageDiff = this.pageCount() - this.limitPerPage;
+        
+        if (pageCountLimitPerPageDiff < 0) { 
+            return 0; 
+        }
+        
+        if (this.page > pageCountLimitPerPageDiff + 1) { 
+            return pageCountLimitPerPageDiff; 
+        } 
+        
+        var low = this.page - (Math.ceil(this.limitPerPage/2) - 1); 
+        
+        return Math.max(low, 0);
+    };
+})
+
+.directive('paginator', function factory() {
+    return {
+        restrict:'E',
+        controller: function ($scope, Paginator) {
+            $scope.paginator = Paginator;
+        },
+        templateUrl: 'paginationControl.html'
+    };
+});
